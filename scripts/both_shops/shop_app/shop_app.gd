abb4c958f6dd7a5d9a9102308b5e7a75e8fd4630
daninatan <danielnatanbrito@hotmail.com>
2025-06-16 21:43:21 -0300

    fix: fixing the search bar and the vbox

--- scripts/both_shops/shop_app/shop_app.gd
+++ scripts/both_shops/shop_app/shop_app.gd
@@ -7,7 +7,6 @@ func _ready() -> void:
 	for button in container.get_children():
 		buttons.append(button)
 	var numero = randi() % 3
-	print(numero)
 	for i in range(3):
 		if i == numero:
 			buttons[i].visible = true
@@ -18,9 +17,9 @@ func _on_open_pressed() -> void:
 	pass
 
 func _on_update_pressed() -> void:
-	buttons[1].text = "Updating..."
+	buttons[1].text = "Atualizando..."
 	await get_tree().create_timer(2.0).timeout
-	buttons[1].text = "Update"
+	buttons[1].text = "Atualizar"
 	buttons[0].visible = true
 	buttons[1].visible = false
 	buttons[2].visible = false
@@ -28,9 +27,9 @@ func _on_update_pressed() -> void:
 
 
 func _on_install_pressed() -> void:
-	buttons[2].text = "Installing..."
+	buttons[2].text = "Instalando..."
 	await get_tree().create_timer(3.0).timeout
-	buttons[2].text = "Install"
+	buttons[2].text = "Instalar"
 	buttons[0].visible = true
 	buttons[1].visible = false
 	buttons[2].visible = false
