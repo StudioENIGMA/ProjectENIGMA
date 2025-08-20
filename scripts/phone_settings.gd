8a05cf249c1352cbe24c91cb1fa024ef6b54b8d1
iLorenzz <ldutra0912@gmail.com>
2025-08-20 14:19:36 -0300

    [0.1.4] FIX :: UNLOCK BASED ON CURRENT DAY Implemented a check of the current date to unlock the corresponding features
    Body:
    (*) "scenes/phone_settings/phone_settings.tscn"
    (*) "scripts/phone_settings.gd"
    
    Footer:
    
    from: [0.1.3]

--- scripts/phone_settings.gd
+++ scripts/phone_settings.gd
@@ -6,20 +6,52 @@ extends Node2D
 @onready var button_os = $Button_OS
 @onready var button_scanner = $Button_scanner
 @onready var passwords_manager = $passwords_manager
+@onready var os_updater = $os_updater
+@onready var close_button = $close
 
-func _ready() -> void:	
+@onready var day : int = 3
+
+func _ready() -> void:
 	button_os.hide()
 	button_scanner.hide()
 		
 	for gui in os_scanner_guis:
-		var style_box_flat = StyleBoxFlat.new()
-		style_box_flat.bg_color = Color(0.72, 0.72, 0.72)
-		gui.get_child(0).add_theme_stylebox_override("panel", style_box_flat)
+		change_gui_color(gui, Color(0.72, 0.72, 0.72), Color(0.36, 0.36, 0.36))
+		#var style_box_flat = StyleBoxFlat.new()
+		#style_box_flat.bg_color = Color(0.72, 0.72, 0.72)
+		#gui.get_child(0).add_theme_stylebox_override("panel", style_box_flat)
 		
-		var style_box_flat2 = StyleBoxFlat.new()
-		style_box_flat2.bg_color = Color(0.36, 0.36, 0.36)
-		gui.get_child(0).get_child(1).add_theme_stylebox_override("panel", style_box_flat2)
+		#var style_box_flat2 = StyleBoxFlat.new()
+		#style_box_flat2.bg_color = Color(0.36, 0.36, 0.36)
+		#gui.get_child(0).get_child(1).add_theme_stylebox_override("panel", style_box_flat2)
+	
+	close_button.pressed.connect(_on_close_button_pressed)
 
+func _process(delta: float) -> void:
+	if day == 2:
+		enable_feature(os_scanner_guis[0], button_os)
+	if day >= 3:
+		enable_feature(os_scanner_guis[0], button_os)
+		enable_feature(os_scanner_guis[1], button_scanner)
+		
+func enable_feature(feature : Control, button : Button) -> void:
+	button.show()
+	change_gui_color(feature, Color(0, 0.7, 0.52), Color(0, 0.34, 0.26))
+	
+func change_gui_color(gui : Control, color1 : Color, color2 : Color) -> void:
+	var style_box_flat = StyleBoxFlat.new()
+	style_box_flat.bg_color = color1
+	gui.get_child(0).add_theme_stylebox_override("panel", style_box_flat)
+	
+	var style_box_flat2 = StyleBoxFlat.new()
+	style_box_flat2.bg_color = color2
+	gui.get_child(0).get_child(1).add_theme_stylebox_override("panel", style_box_flat2)
 
 func _on_button_password_pressed() -> void:
 	passwords_manager.show()
+
+func _on_button_os_pressed() -> void:
+	os_updater.show()
+	
+func _on_close_button_pressed() -> void:
+	self.hide()
