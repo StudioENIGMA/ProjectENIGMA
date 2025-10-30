d8d538d657bccc152352c3cd842509e82be103fd
iLorenzz <ldutra0912@gmail.com>
2025-10-29 23:02:30 -0300

    [0.4.2] REFACTOR :: CHANGING THE SIGNATURE OF ALL VARIABLES AND REMOVING UNNECESSAY CODE
    THE CHANGE WAS MADE IN THE CELLULAR SETTINGS AND IN THE OPERATING SYSTEM UPDATE.
    IT CONSISTS OF SWITCHING THE HARDCODED SIGNATURE OF THE VARIABLES TO EXPORTED
    VARIABLES THAT RECEIVE THE INSTANCE OF THE OBJECTS IN THE SCENE.OBJECTS THAT
    WERE CREATED AND MODIFIED ONLY IN CODE NOW EXIST IN THE SCENE.
    
    Body:
    (*) phone_settings.gd -> all variables of scenes elements are exported and instantiated in the scene.
    (*) phone_settings.tscn -> instantiation of exported variables
    (*) update_os.gd -> all scene element variables are exported. Removal of code style settings.
    (*) update_os.tscn -> instantiation of exported variables
    (+) update_os.tscn -> addition of elements that were created in code, along with the configuration of the font
    
    from: [0.4.1]

--- scripts/apps/settings/phone_settings.gd
+++ scripts/apps/settings/phone_settings.gd
@@ -1,50 +1,44 @@
 extends Node2D
 
-@onready var passwords_gui = $passwordspass
-@onready var os_scanner_guis = [$update_OS, $scanner]
-@onready var button_password = $Button_password
-@onready var button_os = $Button_OS
-@onready var button_scanner = $Button_scanner
-@onready var passwords_manager = $passwords_manager
-@onready var os_updater = $os_updater
-@onready var close_button = $close
-@onready var password_label = $passwords/header/app_name_margin/app_name
-@onready var update_OS_label = $update_OS/header/app_name_margin/app_name
-@onready var scanner_label = $scanner/header/app_name_margin/app_name
+@export var passwords_gui: Control
+@export var os_gui: Control
+@export var scanner_gui: Control
+
+@export var button_password: Button
+@export var button_os: Button
+@export var button_scanner: Button
+@export var close_button: Button
+
+@export var passwords_manager: Node2D
+@export var os_updater: Node2D
+
 @onready var day : int = 2
 
 func _ready() -> void:
 	day = GameData.data.current_day
-	password_label.text = "Gerenciador de Senhas"
-	update_OS_label.text = "Autualização de Software"
-	scanner_label.text = "Verificar Presença de Vírus"
+	passwords_gui.get_child(0).get_child(2).get_child(0).text = "Gerenciador de Senhas"
+	os_gui.get_child(0).get_child(2).get_child(0).text = "Autualização de Software"
+	scanner_gui.get_child(0).get_child(2).get_child(0).text = "Verificar Presença de Vírus"
 	
 	button_os.hide()
 	button_scanner.hide()
 		
-	for gui in os_scanner_guis:
-		change_gui_color(gui, Color(0.72, 0.72, 0.72), Color(0.36, 0.36, 0.36))
-		#var style_box_flat = StyleBoxFlat.new()
-		#style_box_flat.bg_color = Color(0.72, 0.72, 0.72)
-		#gui.get_child(0).add_theme_stylebox_override("panel", style_box_flat)
-		
-		#var style_box_flat2 = StyleBoxFlat.new()
-		#style_box_flat2.bg_color = Color(0.36, 0.36, 0.36)
-		#gui.get_child(0).get_child(1).add_theme_stylebox_override("panel", style_box_flat2)
+	change_gui_color(os_gui, Color(0.72, 0.72, 0.72), Color(0.36, 0.36, 0.36))
+	change_gui_color(scanner_gui, Color(0.72, 0.72, 0.72), Color(0.36, 0.36, 0.36))
 	
 	close_button.pressed.connect(_on_close_button_pressed)
 
 func _process(delta: float) -> void:
 	if day == 2:
-		enable_feature(os_scanner_guis[0], button_os)
+		enable_feature(os_gui, button_os)
 	if day >= 3:
-		enable_feature(os_scanner_guis[0], button_os)
-		enable_feature(os_scanner_guis[1], button_scanner)
-		
+		enable_feature(os_gui, button_os)
+		enable_feature(scanner_gui, button_scanner)
+
 func enable_feature(feature : Control, button : Button) -> void:
 	button.show()
 	change_gui_color(feature, Color(0, 0.7, 0.52), Color(0, 0.34, 0.26))
-	
+
 func change_gui_color(gui : Control, color1 : Color, color2 : Color) -> void:
 	var style_box_flat = StyleBoxFlat.new()
 	style_box_flat.bg_color = color1
