2f6adcabbb66d6f5796d7ab78e663926e46da81a
Lucas Inacio <lucasinacio2003@hotmail.com>
2025-12-17 20:16:26 -0300

    Working without errors

---
+++ scripts/passwords/password_controller.gd
@@ -0,0 +1,51 @@
+extends Controller
+
+signal new_password_created()
+
+const NEW_PASSWORD = preload("res://scenes/phone_settings/change_password.tscn")
+
+@onready var settings_button = get_tree().root.find_child("SettingsButton", true, false)
+@onready var messages_button = get_tree().root.find_child("MessagesButton", true, false)
+@onready var store_button = get_tree().root.find_child("AppStoreButton", true, false)
+@onready var browser_button = get_tree().root.find_child("BrowserButton", true, false)
+@onready var email_button = get_tree().root.find_child("Email", true, false)
+
+@onready var node = get_tree().root.find_child("notifications_control", true, false)
+
+## Initializes the Password Controller and connects app buttons
+func _ready() -> void:
+	var apps_names = AppsControl.apps_name
+	var names_enum = AppsControl.App
+
+	messages_button.pressed.connect(_app_pressed.bindv([apps_names[names_enum.MESSAGES]]))
+	store_button.pressed.connect(_app_pressed.bindv([apps_names[names_enum.STORE]]))
+	browser_button.pressed.connect(_app_pressed.bindv([apps_names[names_enum.BROWSER]]))
+	email_button.pressed.connect(_app_pressed.bindv([apps_names[names_enum.EMAIL]]))
+
+## Handles app button presses
+##
+## app_name: The name of the app whose button was pressed
+func _app_pressed(app_name:String) -> void:
+	if GameData.data.passwords[app_name] == "":
+		_create_password(app_name)
+
+## Creates a new password for the given app
+##
+## app_name: The name of the app for which to create a password
+func _create_password(app_name:String) -> void:
+	var new_password_instance = NEW_PASSWORD.instantiate()
+	node.add_child(new_password_instance)
+	new_password_instance.setup(
+		app_name,
+		ChangePassword.Mode.CREATE
+	)
+
+	new_password_instance.password_confirmed.connect(_on_password_confirmed)
+
+## Handles the event when a new password is created
+##
+## new_password: The newly created password
+## app_name: The name of the app for which the password was created
+func _on_password_confirmed(new_password: String, app_name: String) -> void:
+	GameData.data.passwords[app_name] = new_password
+	new_password_created.emit()
--- scripts/passwords/passwords_manager.gd
+++ scripts/passwords/passwords_manager.gd
@@ -1,71 +1,95 @@
+class_name PasswordsManager
+
 extends Node2D
 
-var vbox_height
+const STORED_PASSWORD_SCENE := preload("res://scenes/phone_settings/password.tscn")
+const CHANGE_PASSWORD_SCENE := preload("res://scenes/phone_settings/change_password.tscn")
 
-@onready var mensagens = $Control/Panel/VBoxContainer/app_password
-@onready var background = $Control/Panel
-@onready var vbox = $Control/Panel/VBoxContainer
-@onready var close_button = $Control/Panel/close
+var none_password_label: Label
+var change_password_ui: ChangePassword
+var stored_by_app: Dictionary = {} # app_name -> StoredPassword
 
-@onready var password_changer = load("res://scenes/phone_settings/change_password.tscn")
-@onready var none_password_label = Label.new()
+@onready var background: Control = $Control/Panel
+@onready var vbox: VBoxContainer = $Control/Panel/VBoxContainer
+@onready var close_button: BaseButton = $Control/Panel/CloseButton
+@onready var password_controller:Controller = $PasswordController
 
 func _ready() -> void:
-	check_passwords()
-
-	if vbox.get_child(0) == null:
-		none_password_added()
-
-	EventBus.first_correct_password.connect(_on_first_password_emitted)
 	close_button.pressed.connect(_on_close_button_pressed)
 
-func check_passwords() -> void:
-	for child in vbox.get_children():
-		child.queue_free()
-
-	for key in GameData.data.passwords.keys():
-		var password = GameData.data.passwords[key]
-		if not password == "":
-			instantiate_password(password, key)
-
-func _process(_delta: float) -> void:
-	pass
-
-func _on_first_password_emitted(new_password:String, app_name:String) -> void:
-	instantiate_password(new_password, app_name)
-	GameData.data.passwords[app_name] = new_password
-	check_passwords()
-	#TODO change icon based in wich app was created
-
-func none_password_added() -> void:
-	background.add_child(none_password_label)
-
+	none_password_label = Label.new()
+	none_password_label.text = "Você não possui nenhuma senha cadastrada"
 	none_password_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
 	none_password_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	none_password_label.custom_minimum_size = Vector2(1000, 200)
-
-	none_password_label.text = "Você não possui nenhuma senha cadastrada"
+	none_password_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	none_password_label.add_theme_font_size_override("font_size", 50)
+	background.add_child(none_password_label)
 
-	var custom_font = FontFile.new()
+	var custom_font := FontFile.new()
 	custom_font.font_data = load("res://assets/fonts/IBMPlexSans-Regular.ttf")
 	none_password_label.add_theme_font_override("font", custom_font)
 
-	label_position(none_password_label)
+	password_controller.new_password_created.connect(_on_new_password_created)
 
-func instantiate_password(new_password:String, app_name:String) -> void:
-	if vbox.get_child(0) == null:
-		none_password_label.hide()
+	_rebuild_list()
 
-	var password_box = load("res://scenes/phone_settings/password.tscn")
-	var password_box_instance = password_box.instantiate()
+func _rebuild_list() -> void:
+	for child in vbox.get_children():
+		child.queue_free()
 
-	vbox.add_child(password_box_instance)
+	stored_by_app.clear()
 
-	password_box_instance.setup(new_password, app_name)
+	var keys: Array = GameData.data.passwords.keys()
+	keys.sort()
 
-func label_position(label:Label) -> void:
-	label.position = Vector2((1080 - label.size.x) / 2, 1000)
+	for app_name in keys:
+		var password_value: String = GameData.data.passwords[app_name]
+		if password_value != "":
+			_add_stored_password_item(app_name, password_value)
 
+	none_password_label.visible = stored_by_app.is_empty()
+
+func _add_stored_password_item(app_name: String, password_value: String) -> void:
+	var item := STORED_PASSWORD_SCENE.instantiate() as StoredPassword
+	vbox.add_child(item)
+
+	item.setup(password_value, app_name)
+	item.change_requested.connect(_on_change_requested)
+
+	stored_by_app[app_name] = item
+
+func _on_change_requested(app_name: String) -> void:
+	_open_change_password(app_name)
+
+func _open_change_password(app_name: String) -> void:
+	# Reuse a single ChangePassword instance (avoids stacking hidden CanvasLayers)
+	if not is_instance_valid(change_password_ui):
+		change_password_ui = CHANGE_PASSWORD_SCENE.instantiate() as ChangePassword
+		_get_overlay_parent().add_child(change_password_ui)
+		change_password_ui.password_confirmed.connect(_on_password_confirmed)
+
+	change_password_ui.setup(app_name, ChangePassword.Mode.CHANGE)
+	change_password_ui.show()
+
+func _on_password_confirmed(new_password: String, app_name: String) -> void:
+	GameData.data.passwords[app_name] = new_password
+	_rebuild_list()
+
+## Gets the appropriate parent node for overlay UIs
+##
+## Returns: The Node to which overlay UIs should be added
+func _get_overlay_parent() -> Node:
+	var overlay := get_tree().root.find_child("notifications_control", true, false)
+	return overlay if overlay != null else get_tree().root
+
+## Handles the close button press
+##
+## Hides the PasswordsManager UI
 func _on_close_button_pressed() -> void:
-	self.hide()
+	hide()
+
+## Handles the event when a new password is created
+##
+## When a new password is created, rebuilds the password list
+func _on_new_password_created() -> void:
+	_rebuild_list()
