1561be5dd77b54999f00be0fc19f73535a4805b4
Lucas Inacio <lucasinacio2003@hotmail.com>
2025-12-17 21:04:15 -0300

    Fix none passwords label

--- scripts/passwords/passwords_manager.gd
+++ scripts/passwords/passwords_manager.gd
@@ -5,34 +5,24 @@ extends Node2D
 const STORED_PASSWORD_SCENE := preload("res://scenes/phone_settings/password.tscn")
 const CHANGE_PASSWORD_SCENE := preload("res://scenes/phone_settings/change_password.tscn")
 
-var none_password_label: Label
 var change_password_ui: ChangePassword
 var stored_by_app: Dictionary = {} # app_name -> StoredPassword
 
-@onready var background: Control = $Control/Panel
+@onready var none_passwords_label: Label = $Control/Panel/NonePasswordsLabel
 @onready var vbox: VBoxContainer = $Control/Panel/VBoxContainer
 @onready var close_button: BaseButton = $Control/Panel/CloseButton
 @onready var password_controller:Controller = $PasswordController
 
+## Initializes the PasswordsManager UI and connects the close button
 func _ready() -> void:
 	close_button.pressed.connect(_on_close_button_pressed)
-
-	none_password_label = Label.new()
-	none_password_label.text = "Você não possui nenhuma senha cadastrada"
-	none_password_label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-	none_password_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	none_password_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	none_password_label.add_theme_font_size_override("font_size", 50)
-	background.add_child(none_password_label)
-
-	var custom_font := FontFile.new()
-	custom_font.font_data = load("res://assets/fonts/IBMPlexSans-Regular.ttf")
-	none_password_label.add_theme_font_override("font", custom_font)
-
 	password_controller.new_password_created.connect(_on_new_password_created)
 
 	_rebuild_list()
 
+## Rebuilds the list of stored passwords in the UI
+##
+## Clears existing items and repopulates the list based on GameData.data.passwords
 func _rebuild_list() -> void:
 	for child in vbox.get_children():
 		child.queue_free()
@@ -47,8 +37,12 @@ func _rebuild_list() -> void:
 		if password_value != "":
 			_add_stored_password_item(app_name, password_value)
 
-	none_password_label.visible = stored_by_app.is_empty()
+	none_passwords_label.visible = vbox.get_child_count() == 0
 
+## Adds a StoredPassword item to the UI for the specified app and password
+##
+## app_name: The name of the app
+## password_value: The password associated with the app
 func _add_stored_password_item(app_name: String, password_value: String) -> void:
 	var item := STORED_PASSWORD_SCENE.instantiate() as StoredPassword
 	vbox.add_child(item)
@@ -58,9 +52,9 @@ func _add_stored_password_item(app_name: String, password_value: String) -> void
 
 	stored_by_app[app_name] = item
 
-func _on_change_requested(app_name: String) -> void:
-	_open_change_password(app_name)
-
+## Opens the ChangePassword UI for the specified app
+##
+## app_name: The name of the app for which to change the password
 func _open_change_password(app_name: String) -> void:
 	# Reuse a single ChangePassword instance (avoids stacking hidden CanvasLayers)
 	if not is_instance_valid(change_password_ui):
@@ -70,10 +64,6 @@ func _open_change_password(app_name: String) -> void:
 
 	change_password_ui.setup(app_name, ChangePassword.Mode.CHANGE)
 	change_password_ui.show()
-
-func _on_password_confirmed(new_password: String, app_name: String) -> void:
-	GameData.data.passwords[app_name] = new_password
-	_rebuild_list()
 
 ## Gets the appropriate parent node for overlay UIs
 ##
@@ -88,8 +78,26 @@ func _get_overlay_parent() -> Node:
 func _on_close_button_pressed() -> void:
 	hide()
 
-## Handles the event when a new password is created
+## Handles the event when a new password is created (from within other parts of the code)
 ##
 ## When a new password is created, rebuilds the password list
 func _on_new_password_created() -> void:
+	_rebuild_list()
+
+## Handles the change password request from a StoredPassword item
+##
+## Opens the ChangePassword UI for the specified app
+##
+## app_name: The name of the app for which the password change was requested
+func _on_change_requested(app_name: String) -> void:
+	_open_change_password(app_name)
+
+## Handles the event when a password is changed
+##
+## Sets the new password for the specified app and rebuilds the password list
+##
+## new_password: The newly changed password
+## app_name: The name of the app for which the password was changed
+func _on_password_confirmed(new_password: String, app_name: String) -> void:
+	GameData.data.passwords[app_name] = new_password
 	_rebuild_list()
