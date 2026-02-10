0c63de95bf14871c61912ff0484c3706444e0c11
Nmora511 <jomoraesneto@gmail.com>
2026-02-10 01:53:43 -0300

    feat: add company card scene and script

---
+++ scripts/apps/browser/company_card.gd
@@ -0,0 +1,18 @@
+extends PanelContainer
+
+@export var score_label: Label
+@export var name_label: Label
+@export var summary_label: Label
+
+func setup(company_score: int, company_name: String, reviews_summary: String) -> void:
+	name_label.text = company_name
+	summary_label.text = "Resumo: %s" % reviews_summary
+	score_label.text = str(company_score)
+
+	var current_style: StyleBoxFlat = score_label.get_theme_stylebox("normal").duplicate()
+	if company_score >= 50:
+		current_style.bg_color = Color("#196153")
+	else:
+		current_style.bg_color = Color("#660f31")
+
+	score_label.add_theme_stylebox_override("normal", current_style)
