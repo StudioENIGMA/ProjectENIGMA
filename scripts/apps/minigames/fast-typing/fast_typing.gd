709f3b253eed29faf91a4b6609996662d35e01b6
iLorenzz <ldutra0912@gmail.com>
2026-02-16 00:19:31 -0300

    fix: changed default label to rich text label

--- scripts/apps/minigames/fast-typing/fast_typing.gd
+++ scripts/apps/minigames/fast-typing/fast_typing.gd
@@ -2,19 +2,26 @@ extends Control
 
 
 #region CHILDREN NODES REFERENCES
-@export var text_to_type1: Label
-@export var text_to_type2: Label
+@export var text_to_type1: RichTextLabel
+@export var text_to_type2: RichTextLabel
 @export var player_type_space: LineEdit
 @export var minigame_timer: MarginContainer
 #endregion
 
-
+var bytes_of_text_to_type1: PackedByteArray
+var bytes_of_text_to_type2: PackedByteArray
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	bytes_of_text_to_type1 = text_to_type1.text.to_utf8_buffer()
+	bytes_of_text_to_type2 = text_to_type1.text.to_utf8_buffer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func verify_correct_text() -> void:
+	var bytes_of_player_text: PackedByteArray = player_type_space.text.to_utf8_buffer()
+	
+
