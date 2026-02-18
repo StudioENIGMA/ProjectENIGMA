9641327f27d9820e9d7e2f9d75d359a85169af2d
iLorenzz <ldutra0912@gmail.com>
2026-02-18 15:24:50 -0300

    saving

--- scripts/apps/minigames/fast-typing/fast_typing.gd
+++ scripts/apps/minigames/fast-typing/fast_typing.gd
@@ -10,6 +10,7 @@ extends Control
 
 var bytes_of_text_to_type1: PackedByteArray
 var bytes_of_text_to_type2: PackedByteArray
+var first_completed: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,5 +24,10 @@ func _process(_delta: float) -> void:
 
 func verify_correct_text() -> void:
 	var bytes_of_player_text: PackedByteArray = player_type_space.text.to_utf8_buffer()
+	for i in range(player_type_space.text.length()):
+		if first_completed:
+			if bytes_of_text_to_type2[i] == bytes_of_player_text[i]:
+				
+
 	
 
