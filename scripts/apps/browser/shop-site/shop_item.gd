42cb5588357e499a474d845e24d4d87711c0a1f4
Nmora511 <jomoraesneto@gmail.com>
2026-02-20 18:34:14 -0300

    feat: modify shop item scene and script

--- scripts/apps/browser/shop-site/shop_item.gd
+++ scripts/apps/browser/shop-site/shop_item.gd
@@ -1,18 +1,34 @@
 extends Control
 
-var quantity_btn
+signal add_to_cart(item_data: Dictionary)
 
+@export var item_quantity_label: Label
+var item_quantity: int = 1
+var item_data: Dictionary
+
+# FUNCTION BEING USED FOR MOCK VALUES
 func _ready() -> void:
-	quantity_btn = $Panel/Panel/item_quantity
+	item_data["name"] = "Nome do Produto"
+	item_data["price"] = 130
+	item_data["logo"] = "res://assets/icons/shops/products/camiseta-placeholder.png"
+
+func setup(shop_item_data: Dictionary) -> void:
+	item_data = shop_item_data
+
+func update_quantity_label() -> void:
+	item_quantity_label.text = str(item_quantity) 
 
 func _on_add_item_pressed() -> void:
-	var quantity = int(quantity_btn.text)
-	quantity += 1
-	quantity_btn.text = str(quantity)
-
+	item_quantity += 1
+	update_quantity_label()
 
 func _on_remove_item_pressed() -> void:
-	var quantity = int(quantity_btn.text)
-	if quantity > 0:
-		quantity -= 1
-		quantity_btn.text = str(quantity)
+	if(item_quantity > 1):
+		item_quantity -= 1
+		update_quantity_label()
+
+func _on_add_to_cart_pressed() -> void:
+	item_data["quantity"] = item_quantity
+	add_to_cart.emit(item_data)
+	item_quantity = 1
+	update_quantity_label()
