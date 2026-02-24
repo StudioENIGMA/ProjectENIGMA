80fff432b192dc09fb0b606eab8c1cfdf6b783fe
Nmora511 <jomoraesneto@gmail.com>
2026-02-24 20:20:41 -0300

    feat: make shop_item dynamic

--- scripts/apps/browser/shop-site/shop_item.gd
+++ scripts/apps/browser/shop-site/shop_item.gd
@@ -2,21 +2,21 @@ extends Control
 
 signal add_to_cart(item_data: Dictionary)
 
+@export var item_name_label: Label
+@export var item_price_label: Label
+@export var item_icon_rect: TextureRect
 @export var item_quantity_label: Label
 var item_quantity: int = 1
 var item_data: Dictionary
 
-# FUNCTION BEING USED FOR MOCK VALUES
-func _ready() -> void:
-	item_data["name"] = "Nome do Produto"
-	item_data["price"] = 130
-	item_data["logo"] = "res://assets/icons/shops/products/camiseta-placeholder.png"
-
 func setup(shop_item_data: Dictionary) -> void:
 	item_data = shop_item_data
+	item_name_label.text = item_data["name"]
+	item_price_label.text = GameData.format_brl(item_data["price"])
+	item_icon_rect.texture = load(item_data["logo"])
 
 func update_quantity_label() -> void:
-	item_quantity_label.text = str(item_quantity) 
+	item_quantity_label.text = str(item_quantity)
 
 func _on_add_item_pressed() -> void:
 	item_quantity += 1
@@ -29,6 +29,6 @@ func _on_remove_item_pressed() -> void:
 
 func _on_add_to_cart_pressed() -> void:
 	item_data["quantity"] = item_quantity
-	add_to_cart.emit(item_data)
+	add_to_cart.emit(item_data.duplicate(true))
 	item_quantity = 1
 	update_quantity_label()
