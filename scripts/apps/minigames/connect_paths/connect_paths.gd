23ab7aefdbb34c6650258fd524128b5255d9d1fe
daninatan <danielnatanbrito@hotmail.com>
2026-02-26 16:08:37 -0300

    feat: creating the hexagon and minigame scene

---
+++ scripts/apps/minigames/connect_paths/connect_paths.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@export var mid_hex : Node2D
+@export var ul_hex : Node2D
+@export var ur_hex : Node2D
+@export var l_hex : Node2D
+@export var r_hex : Node2D
+@export var ll_hex : Node2D
+@export var lr_hex : Node2D
+
+
