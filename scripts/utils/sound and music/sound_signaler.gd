e81fb717b8c9627a03f273639c3f9df764122800
dino460 <raphael.z.cali@gmail.com>
2026-03-03 18:20:10 -0300

    FEAT :: SOUND DIRECTOR
    Added code to handle playing music procedurally.
    
    Body:
    (+) Bunch of music sound samples
    (+) Created signals to help control the music.
    (+) Created Instrument script to hold info about each instrument.
    (+) Created Music script to hold info about each pre-made music.
    (*) Updated sound_handler.gd to sound_director.gd.
    
    Footer:
    This update is focused on removing the old simplistic 'load and play' system into a more advanced procedural music system.
    The director counts a predefined number of ticks and enables music to be played at appropriate intervals according to predefined bar size and tempo.
    This allow stems to be properly synced, as long as they are the size of the bar, or half of it.
    The director also allows for explicit declaration of the order in which stems for any given instrument must be played.
    
    from: [<old-version>]

---
+++ scripts/utils/sound and music/sound_signaler.gd
@@ -0,0 +1,13 @@
+extends Node
+
+class_name SoundSignaler
+
+signal set_melody_modulation(value : bool)
+
+signal set_instrument(instrument_id : int, value : bool)
+
+signal set_double_tempo(double_tempo : bool)
+
+signal set_music(music : Music)
+
+signal set_should_generate_music(value : bool)
