6ebb5c93fbcaa2a2858c45324c4cdc0ada19a1bb
Nmora511 <jomoraesneto@gmail.com>
2026-03-03 19:02:08 -0300

    feat: add aec to browser

--- scripts/apps/browser/quick_acess_icon.gd
+++ scripts/apps/browser/quick_acess_icon.gd
@@ -18,10 +18,11 @@ var logo_by_enum = {
 	GameData.App.BROWSERAMAZONIASHOP: preload("res://assets/icons/shops/square_logos/amazonia.png"),
 }
 
-var name_by_enum = {
+var sites_names_by_enum = {
 	GameData.App.REVIEWSSITE: "ElogieLÁ",
 	GameData.App.BROWSERAMAZONIASHOP: "Amazônia",
-	GameData.App.BROWSEREMILIASHOP: "Emília Bolos"
+	GameData.App.BROWSEREMILIASHOP: "Emília Bolos",
+	GameData.App.BROWSERAECSHOP: "A&C"
 }
 
 func _ready() -> void:
@@ -31,7 +32,7 @@ func update_display() -> void:
 	if not is_inside_tree() or not site_label or not logo_button:
 		return
 
-	site_label.text = name_by_enum.get(site, "Unknown")
+	site_label.text = sites_names_by_enum.get(site, "Unknown")
 	logo_button.texture_normal = logo_by_enum.get(site, DEFAULT_LOGO)
 
 func _on_texture_button_pressed() -> void:
