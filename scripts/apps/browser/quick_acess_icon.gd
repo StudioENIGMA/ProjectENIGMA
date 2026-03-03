99738e127035fe51d756a8cc0d25b1fbd2def874
Nmora511 <jomoraesneto@gmail.com>
2026-03-03 14:00:55 -0300

    feat: add quick acess icon tool

--- scripts/apps/browser/quick_acess_icon.gd
+++ scripts/apps/browser/quick_acess_icon.gd
@@ -1,17 +1,38 @@
+@tool
 extends Control
 
 signal open_site_requested(app: GameData.App)
 
-@export var site_name: String
-@export var logo_url: String
-@export var app: GameData.App
+const DEFAULT_LOGO =  preload("res://assets/projeto enigma.png")
+
+@export var site: GameData.App:
+	set(value):
+		site = value
+		_ready()
 
 @export var logo_button: TextureButton
 @export var site_label: Label
 
+var logo_by_enum = {
+	GameData.App.REVIEWSSITE: preload("res://assets/icons/shops/square_logos/elogie_la.png"),
+	GameData.App.BROWSERAMAZONIASHOP: preload("res://assets/icons/shops/square_logos/amazonia.png"),
+}
+
+var name_by_enum = {
+	GameData.App.REVIEWSSITE: "ElogieLÁ",
+	GameData.App.BROWSERAMAZONIASHOP: "Amazônia",
+	GameData.App.BROWSEREMILIASHOP: "Emília Bolos"
+}
+
 func _ready() -> void:
-	site_label.text = site_name
-	logo_button.texture_normal = load(logo_url)
+	update_display()
+
+func update_display() -> void:
+	if not is_inside_tree() or not site_label or not logo_button:
+		return
+
+	site_label.text = name_by_enum.get(site, "Unknown")
+	logo_button.texture_normal = logo_by_enum.get(site, DEFAULT_LOGO)
 
 func _on_texture_button_pressed() -> void:
-	open_site_requested.emit(app)
+	open_site_requested.emit(site)
