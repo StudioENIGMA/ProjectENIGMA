4b1d41fc3d69e8caf6ed53334a1b788e6641b320
daninatan <danielnatanbrito@hotmail.com>
2026-03-04 16:44:24 -0300

    feat: randomizing hexagon rotation and checking when finished

--- scripts/apps/minigames/connect_paths/connect_paths.gd
+++ scripts/apps/minigames/connect_paths/connect_paths.gd
@@ -9,14 +9,26 @@ extends Control
 @export var lr_hex : Node2D
 @export var path_generator : Node
 
+
 var hexagons = []
 var hexagons_node = []
+var hexagons_positions = [1, 1, 1, 1, 1, 1, 1]
 
 func _ready() -> void:
 	path_generator.hexagons_generated.connect(get_hexagon_path)
+	mid_hex.rotation_click.connect(update_rotation.bind(0))
+	ul_hex.rotation_click.connect(update_rotation.bind(1))
+	ur_hex.rotation_click.connect(update_rotation.bind(2))
+	r_hex.rotation_click.connect(update_rotation.bind(3))
+	lr_hex.rotation_click.connect(update_rotation.bind(4))
+	ll_hex.rotation_click.connect(update_rotation.bind(5))
+	l_hex.rotation_click.connect(update_rotation.bind(6))
+
 	path_generator.generate()
 	create_hexagons_node_array()
 	set_lines()
+	randomize_rotation()
+
 
 func get_hexagon_path(hexagon_array):
 	hexagons = hexagon_array
@@ -47,3 +59,50 @@ func create_hexagons_node_array():
 	hexagons_node.append(lr_hex)
 	hexagons_node.append(ll_hex)
 	hexagons_node.append(l_hex)
+
+func randomize_rotation():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	for i in range(hexagons.size()):
+		var rotations = rng.randi_range(0, 5)
+		if not hexagons[i]["vertex_number"] == 0:
+			hexagons_positions[i] = rotations + 1
+			hexagons_node[i].rotation = rotations * deg_to_rad(60)
+
+func update_rotation(index):
+	if not hexagons[index]["vertex_number"] == 0:
+		if not hexagons_positions[index] == 6:
+			hexagons_positions[index] += 1
+		else:
+			hexagons_positions[index] = 1
+
+	check_rotations()
+
+func get_rotation_period(vertex_positions: Array) -> int:
+	var base = vertex_positions.duplicate()
+	base.sort()
+
+	for step in range(1, 6):
+		var rotated = []
+		for v in vertex_positions:
+			var new_v = v + step
+			if new_v > 6:
+				new_v -= 6
+			rotated.append(new_v)
+
+		rotated.sort()
+
+		if rotated == base:
+			return step
+
+	return 6
+
+func check_rotations():
+	var check = true
+	for i in range(0, 7):
+		var period = get_rotation_period(hexagons[i]["vertex_positions"])
+		if not (hexagons_positions[i] - 1) % period == 0:
+			check = false
+	if check:
+		print("concluiu minigame")
+		
--- scripts/apps/minigames/connect_paths/hex_tile.gd
+++ scripts/apps/minigames/connect_paths/hex_tile.gd
@@ -7,7 +7,10 @@ extends Area2D
 @export var line5: Sprite2D
 @export var line6: Sprite2D
 
+signal rotation_click
+
 func _input_event(viewport, event, shape_idx):
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
             rotation += deg_to_rad(60)
+            rotation_click.emit()
