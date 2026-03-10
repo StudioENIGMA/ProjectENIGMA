b6764be1d604feb290c55170464ff436ea2a1ac2
Nmora511 <jomoraesneto@gmail.com>
2026-03-10 15:38:54 -0300

    fix: fix script typo

--- scripts/utils/max_size_label.gd
+++ scripts/utils/max_size_label.gd
@@ -1,7 +1,7 @@
 extends Label
 
 const DEFAULT_SIZE: int = 30
-@export var MAX_WIDTH: float = 50
+@export var max_width: float = 50
 
 func _ready() -> void:
 	visibility_changed.connect(_on_update_font_size)
@@ -9,7 +9,6 @@ func _ready() -> void:
 func _on_update_font_size() -> void:
 	var current_font_size = DEFAULT_SIZE
 	add_theme_font_size_override("font_size", current_font_size)
-	while get_minimum_size().x > MAX_WIDTH and current_font_size > 5:
+	while get_minimum_size().x > max_width and current_font_size > 5:
 		current_font_size -= 1
 		add_theme_font_size_override("font_size", current_font_size)
-	print(current_font_size)
