8b4bb5717beb82aefe6345a3932907662ea69cda
Nmora511 <jomoraesneto@gmail.com>
2026-03-10 15:57:02 -0300

    refact: change 2 digit date calculator

--- scripts/apps/messages/time_indicator.gd
+++ scripts/apps/messages/time_indicator.gd
@@ -6,14 +6,4 @@ func setup(date_dict: Dictionary) -> void:
 	if date_dict["day"] == GameData.get_current_date_dict()["day"]:
 		time_label.text = "Hoje"
 	else:
-		var day_string = "%d" % date_dict["day"]
-		@warning_ignore("integer_division")
-		if int(date_dict["day"]) / int(10) < 1:
-			day_string = "0" + day_string
-
-		var month_string = "%d" % date_dict["month"]
-		@warning_ignore("integer_division")
-		if int(date_dict["month"]) / int(10) < 1:
-			month_string = "0" + month_string
-
-		time_label.text = "%s/%s" % [day_string, month_string]
+		time_label.text = "%02d/%02d" % [date_dict["day"], date_dict["month"]]
