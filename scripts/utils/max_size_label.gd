e78ece621cef8727d956de1af8df37ea86d88933
Nmora511 <jomoraesneto@gmail.com>
2026-03-10 00:46:00 -0300

    feat: add dynamic label resize script

---
+++ scripts/utils/max_size_label.gd
@@ -0,0 +1,13 @@
+extends Label
+
+const DEFAULT_SIZE: int = 40
+@export var MAX_WIDTH: float = 50
+
+func _ready() -> void:
+	visibility_changed.connect(_on_update_font_size)
+
+func _on_update_font_size() -> void:
+	var current_font_size = DEFAULT_SIZE
+	while get_minimum_size().x > MAX_WIDTH and current_font_size > 5:
+		current_font_size -= 1
+		add_theme_font_size_override("font_size", current_font_size)
