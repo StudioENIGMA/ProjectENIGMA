a0ab0ad49382077dd266f97bc7fcf2f6af38bc29
Nmora511 <jomoraesneto@gmail.com>
2026-03-10 14:33:29 -0300

    fix: fix size error

--- scripts/utils/max_size_label.gd
+++ scripts/utils/max_size_label.gd
@@ -1,6 +1,6 @@
 extends Label
 
-const DEFAULT_SIZE: int = 40
+const DEFAULT_SIZE: int = 30
 @export var MAX_WIDTH: float = 50
 
 func _ready() -> void:
@@ -8,6 +8,8 @@ func _ready() -> void:
 
 func _on_update_font_size() -> void:
 	var current_font_size = DEFAULT_SIZE
+	add_theme_font_size_override("font_size", current_font_size)
 	while get_minimum_size().x > MAX_WIDTH and current_font_size > 5:
 		current_font_size -= 1
 		add_theme_font_size_override("font_size", current_font_size)
+	print(current_font_size)
