3329c0be7ced16e8e1c441a43e310d208d0e32fe
Nmora511 <jomoraesneto@gmail.com>
2026-03-09 23:21:26 -0300

    feat: add time indicator script and scene

--- scripts/apps/messages/time_indicator.gd
+++ scripts/apps/messages/time_indicator.gd
@@ -3,5 +3,17 @@ extends Control
 @export var time_label: Label
 
 func setup(date_dict: Dictionary) -> void:
-	#if date_dict["day"] == GameDat
-	time_label.text = "%s/%s" % [date_dict["day"], date_dict["month"]]
+	if date_dict["day"] == GameData.get_current_date_dict()["day"]:
+		time_label.text = "Hoje"
+	else:
+		var day_string = "%d" % date_dict["day"]
+		@warning_ignore("integer_division")
+		if int(date_dict["day"]) / int(10) < 1:
+			day_string = "0" + day_string
+
+		var month_string = "%d" % date_dict["month"]
+		@warning_ignore("integer_division")
+		if int(date_dict["month"]) / int(10) < 1:
+			month_string = "0" + month_string
+
+		time_label.text = "%s/%s" % [day_string, month_string]
