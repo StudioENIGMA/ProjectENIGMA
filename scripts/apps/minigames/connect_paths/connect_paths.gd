d79e96105f4cc7e497dabbad0249e8d30fce7c28
Lucas Inacio <lucasinacio2003@hotmail.com>
2026-03-09 22:42:38 -0300

    Temporary fix

--- scripts/apps/minigames/connect_paths/connect_paths.gd
+++ scripts/apps/minigames/connect_paths/connect_paths.gd
@@ -2,13 +2,13 @@ extends Control
 
 signal hack_concluded
 
-@export var mid_hex : Node2D
-@export var ul_hex : Node2D
-@export var ur_hex : Node2D
-@export var l_hex : Node2D
-@export var r_hex : Node2D
-@export var ll_hex : Node2D
-@export var lr_hex : Node2D
+@export var mid_hex : TextureButton
+@export var ul_hex : TextureButton
+@export var ur_hex : TextureButton
+@export var l_hex : TextureButton
+@export var r_hex : TextureButton
+@export var ll_hex : TextureButton
+@export var lr_hex : TextureButton
 @export var path_generator : Node
 @export var minigame_timer : ProgressBar
 
@@ -27,9 +27,9 @@ func _ready() -> void:
 	ll_hex.rotation_click.connect(update_rotation.bind(5))
 	l_hex.rotation_click.connect(update_rotation.bind(6))
 	setup()
-	
 
-func setup() -> void :
+func setup() -> void:
+	hexagons_positions = [1, 1, 1, 1, 1, 1, 1]
 	path_generator.generate()
 	create_hexagons_node_array()
 	set_lines()
@@ -39,44 +39,38 @@ func setup() -> void :
 func get_hexagon_path(hexagon_array):
 	hexagons = hexagon_array
 
-func set_lines():
+func set_lines() -> void:
 	for i in range(hexagons.size()):
 		var hexagon_data = hexagons[i]
 		var hexagon_node = hexagons_node[i]
+		hexagon_node.set_active_lines(hexagon_data["vertex_positions"])
 
-		var active_lines = hexagon_data["vertex_positions"]
+func create_hexagons_node_array() -> void:
+	hexagons_node = [
+		mid_hex,
+		ul_hex,
+		ur_hex,
+		r_hex,
+		lr_hex,
+		ll_hex,
+		l_hex,
+	]
 
-		# Primeiro desativa todas
-		for j in range(1, 7):
-			hexagon_node.get("line" + str(j)).visible = false
-
-		# Depois ativa só as que estão no array
-		for line_number in active_lines:
-			hexagon_node.get("line" + str(line_number)).visible = true
-
-
-
-func create_hexagons_node_array():
-	hexagons_node.append(mid_hex)
-	hexagons_node.append(ul_hex)
-	hexagons_node.append(ur_hex)
-	hexagons_node.append(r_hex)
-	hexagons_node.append(lr_hex)
-	hexagons_node.append(ll_hex)
-	hexagons_node.append(l_hex)
-
-func randomize_rotation():
+func randomize_rotation() -> void:
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
+
 	for i in range(hexagons.size()):
+		hexagons_node[i].reset_rotation()
+
 		var rotations = rng.randi_range(0, 5)
-		if not hexagons[i]["vertex_number"] == 0:
+		if hexagons[i]["vertex_number"] != 0:
 			hexagons_positions[i] = rotations + 1
-			hexagons_node[i].rotation = rotations * deg_to_rad(60)
+			hexagons_node[i].set_rotation_steps(rotations)
 
 func update_rotation(index):
-	if not hexagons[index]["vertex_number"] == 0:
-		if not hexagons_positions[index] == 6:
+	if hexagons[index]["vertex_number"] != 0:
+		if hexagons_positions[index] != 6:
 			hexagons_positions[index] += 1
 		else:
 			hexagons_positions[index] = 1
@@ -122,4 +116,3 @@ func check_rotations():
 
 func _on_time_finished() -> void:
 	setup()
-		
