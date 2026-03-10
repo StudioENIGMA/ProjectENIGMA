77e19d63da645f7159216ff6ff22ec8a4bd1691e
Lucas Inacio <lucasinacio2003@hotmail.com>
2026-03-10 09:07:31 -0300

    Days passing

--- scripts/singletons/game_data.gd
+++ scripts/singletons/game_data.gd
@@ -87,7 +87,7 @@ var bank_balance: float = 200
 var start_date_dict: Dictionary # {year, month, day, weekday}
 var starting_hours_minutes:int = 600	# Start at 6:00
 var hours_minutes:int = 600 # This one will increase with time
-var max_hours_minutes:int = 1200 # End at 12:00
+var max_hours_minutes:int = 605 # End at 12:00
 var current_day:int = 0
 var reputation_points:int = 0
 var authentication_codes: Dictionary = {} # GameData.App as key, code as value
@@ -218,8 +218,8 @@ var apps_chinese_operations = {
 	"open": "阿布里爾"
 }
 
-var apps_in_store: Array[App] = [App.MESSAGESHOME, App.EMAIL, App.BROWSER]
-var downloaded_apps: Array[App] = [App.MESSAGESHOME, App.BROWSER]
+var apps_in_store: Array[App] = [App.MESSAGESHOME, App.EMAIL]
+var downloaded_apps: Array[App] = [App.MESSAGESHOME]
 var available_updates: Array[App] = []
 
 func _ready() -> void:
--- scripts/utils/day_over_ui.gd
+++ scripts/utils/day_over_ui.gd
@@ -4,14 +4,15 @@ signal day_over_clicked()
 
 @export var next_day_button:Button
 
+func _ready() -> void:
+	next_day_button.pressed.connect(_on_next_day_button_pressed)
+
 func show_day_over() -> void:
 	self.visible = true
 	next_day_button.text = "Iniciar Dia " + str(GameData.current_day + 1)
-	next_day_button.pressed.connect(_on_next_day_button_pressed)
 
 func hide_day_over() -> void:
 	self.visible = false
 
 func _on_next_day_button_pressed() -> void:
-	GameData.current_day += 1
 	day_over_clicked.emit()
