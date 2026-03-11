36b9af56034456a762850c53ed2d949779dcfa6b
Nmora511 <jomoraesneto@gmail.com>
2026-03-11 11:12:41 -0300

    fix: reorganize items in UI

--- scripts/apps/settings/update_os.gd
+++ scripts/apps/settings/update_os.gd
@@ -10,12 +10,11 @@ func _ready() -> void:
 
 func _on_update_button_pressed() -> void:
 	GameData.updated_os_today = true
-	update_button.text = "Sistema atualizado hoje"
+	update_button.text = "Atualizando Sistema"
 	update_button.disabled = true
 	animator.play("update_os")
 
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	# Use to detect when update is over (at the end of the animation)
-	# Replace with function body.
-	pass
+	update_button.text = "Sistema atualizado hoje"
