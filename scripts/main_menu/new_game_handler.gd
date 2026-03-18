3f799404d1c3e087d57c6b3a660b9f0111335f2a
dino460 <raphael.z.cali@gmail.com>
2026-03-18 11:32:48 -0300

    Fixed music no persisting on starting game

--- scripts/main_menu/new_game_handler.gd
+++ scripts/main_menu/new_game_handler.gd
@@ -1,14 +1,22 @@
 extends Button
 
-var new_game_scene:Resource
+var new_game_scene : Control
 
 func _ready() -> void:
-	new_game_scene = preload("res://scenes/game_screen.tscn")
+	new_game_scene = preload("res://scenes/game_screen.tscn").instantiate()
+	get_tree().root.add_child.call_deferred(new_game_scene)
+	new_game_scene.visible = false
+	new_game_scene.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _on_pressed() -> void:
 	GameData.start_date_dict = Time.get_date_dict_from_system()
-	change_scene()
+	print(get_tree().root.get_children())
+	var menu : Control = get_tree().root.get_child(1)
+	menu.visible = false
+	new_game_scene.visible = true
+	new_game_scene.process_mode = Node.PROCESS_MODE_ALWAYS
+# 	change_scene()
 
-func change_scene() -> void:
-	new_game_scene.instantiate()
-	get_tree().change_scene_to_packed(new_game_scene)
+# func change_scene() -> void:
+# 	new_game_scene.instantiate()
+# 	get_tree().change_scene_to_packed(new_game_scene)
