ddb0d560b000cde0d7c304766d276bcd2b2e2dcb
Nmora511 <jomoraesneto@gmail.com>
2026-03-19 15:24:36 -0300

    feat: change minigames connection

--- scripts/apps/base_app.gd
+++ scripts/apps/base_app.gd
@@ -250,13 +250,13 @@ func _ready() -> void:
 	# Hack minigame maze (Hack minigames)
 	maze.visible = false
 	maze.hack_concluded.connect(_on_back_button_pressed)
-	maze.hack_concluded.connect(virus_scanner._on_scan_timer_timeout)
+	maze.hack_concluded.connect(virus_scanner._on_video_stream_finished)
 	hack_screen.add_child(maze)
 
 	# Hack minigame line connect (Hack minigames)
 	line_connect.visible = false
 	line_connect.hack_concluded.connect(_on_back_button_pressed)
-	line_connect.hack_concluded.connect(virus_scanner._on_scan_timer_timeout)
+	line_connect.hack_concluded.connect(virus_scanner._on_video_stream_finished)
 	hack_screen.add_child(line_connect)
 
 ## Handles the app opened event from the desktop UI
