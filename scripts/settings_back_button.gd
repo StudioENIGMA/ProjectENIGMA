f00c90c89aafdeb0fda2164904ff3e048f08b2e0
dino460 <raphael.z.cali@gmail.com>
2026-03-20 15:20:22 -0300

    Fixed menu settings stopping music and settings not working

--- scripts/settings_back_button.gd
+++ scripts/settings_back_button.gd
@@ -1,6 +1,6 @@
 extends Button
 
-const MAIN_MENU_SCENE_PATH = "res://scenes/main_menu.tscn"
-
 func _pressed() -> void:
-	get_tree().change_scene_to_file(MAIN_MENU_SCENE_PATH)
+	self.get_parent().get_parent().visible = false
+	var menu : Control = get_tree().root.get_child(1)
+	menu.visible = true
--- scripts/utils/sound and music/sound_director.gd
+++ scripts/utils/sound and music/sound_director.gd
@@ -339,26 +339,6 @@ func modulate_melody(enabled : bool, time : float):
 		AudioServer.get_bus_effect(AudioServer.get_bus_index("Modulated"), 0).set("cutoff_hz", cutoff_hz)
 
 
-
-func _on_mute_sfx_check_box_toggled(toggled_on:bool) -> void:
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), toggled_on)
-
-func _on_mute_music_check_box_toggled(toggled_on:bool) -> void:
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), toggled_on)
-
-func _on_mute_master_check_box_toggled(toggled_on:bool) -> void:
-	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
-
-
-func _on_sfx_volume_h_slider_value_changed(value:float) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), convert_float_to_db(value))
-
-func _on_music_volume_h_slider_value_changed(value:float) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), convert_float_to_db(value))
-
-func _on_master_volume_h_slider_value_changed(value:float) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), convert_float_to_db(value))
-
 func convert_float_to_db(value:float) -> float:
 	var ret:float
 	if value <= 0.1:
