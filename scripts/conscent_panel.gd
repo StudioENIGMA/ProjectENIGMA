9beb14960e5031f602414bd714aefb4d37d6e810
Nmora511 <jomoraesneto@gmail.com>
2026-04-14 12:34:53 -0300

    feat: add conscent message persistent data

--- scripts/conscent_panel.gd
+++ scripts/conscent_panel.gd
@@ -3,7 +3,18 @@ extends PanelContainer
 @export var conscent_button: Button
 
 func _ready() -> void:
-  conscent_button.pressed.connect(_on_conscent_button_pressed)
+	var file_path := "user://age_verification.flag"
+	if not FileAccess.file_exists(file_path):
+		show()
+	conscent_button.pressed.connect(_on_conscent_button_pressed)
 
 func _on_conscent_button_pressed() -> void:
-  hide()
+	var file_path = "user://age_verification.flag"
+	var age_file = FileAccess.open(file_path, FileAccess.WRITE)
+	if age_file == null:
+		push_error("Could not open save file for writing")
+		return
+
+	age_file.store_string("")
+	age_file.close()
+	hide()
