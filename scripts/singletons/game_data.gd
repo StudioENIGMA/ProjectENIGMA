e785c192509b7d89b602ac6f0600e6bb1e4b133c
Lucas Inacio <lucasinacio2003@hotmail.com>
2026-05-09 01:35:46 -0300

    Change hours

--- scripts/singletons/game_data.gd
+++ scripts/singletons/game_data.gd
@@ -115,17 +115,15 @@ var bank_balance: float = 200
 var completed_payments = []
 var purchased_items = {}
 var options_chose = {}
- 
+
 var random_events_history = []
 
 var start_date_dict: Dictionary # {year, month, day, weekday}
-var starting_hours_minutes:int = 600	# Start at 10:00
-var hours_minutes:int = 600 # This one will increase with time
-var max_hours_minutes:int = 960 # End at 16:00
-var current_day:int = 1
-var daily_reputation_points:int = 0
-var total_reputation_points:int = 0
-var events_log = []
+var starting_hours_minutes:int = 480	# Start at 08:00
+var hours_minutes:int = 480 # This one will increase with time
+var max_hours_minutes:int = 1080 # End at 18:00
+var current_day:int = 0
+var reputation_points:int = 0
 var authentication_codes: Dictionary = {} # GameData.App as key, code as value
 var passwords: Dictionary = {
 	App.BANK: str(randi_range(0, 9999)).pad_zeros(4), # Random default password each time
