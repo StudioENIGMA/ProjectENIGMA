d650f6b703fef698cfa90011a32c2633b824590b
Nmora511 <jomoraesneto@gmail.com>
2026-05-10 19:18:36 -0300

    feat: add potetial apps variable to gamedata

--- scripts/singletons/game_data.gd
+++ scripts/singletons/game_data.gd
@@ -281,6 +281,7 @@ var apps_chinese_operations = {
 	"open": "阿布里爾"
 }
 
+var potential_apps_in_store = [App.MESSAGESHOME, App.BROWSER, App.EMAIL, App.BANK]
 var apps_in_store: Array[App] = [App.MESSAGESHOME]
 var downloaded_apps: Array[App] = [App.MESSAGESHOME]
 
