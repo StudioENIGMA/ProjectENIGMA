4476b2db9ccc7ada97730b42933428aa99a0bf3c
Nmora511 <jomoraesneto@gmail.com>
2026-06-10 23:32:06 -0300

    feat: add screen fix for virtual keyboard

--- scripts/game_screen.gd
+++ scripts/game_screen.gd
@@ -11,6 +11,8 @@ extends Control
 @export var progressive_blur: ColorRect
 #endregion CHILDREN NODES REFERENCES
 
+@onready var base_window_height : int = int(ProjectSettings.get_setting("display/window/size/viewport_height"))
+
 #region INITIALIZATION
 ## Setup signal connections to redirect events to each app
 func _ready() -> void:
@@ -94,6 +96,19 @@ func _ready() -> void:
   ui.base_app.pause_game_requested.connect(ui.pause_game_ui.show_pause_menu)
   ui.day_over_ui.end_game.connect(_on_end_game)
 #endregion INITIALIZATION
+
+func _process(_delta: float) -> void:
+  var keyboard_height : int = DisplayServer.virtual_keyboard_get_height()
+
+  if keyboard_height > 0:
+    var window_size : Vector2i = DisplayServer.window_get_size()
+    var scale_factor : float = float(base_window_height) / float(window_size.y)
+
+    var scaled_keyboard_height : float = keyboard_height * scale_factor
+
+    position.y = -scaled_keyboard_height
+  else:
+    position.y = 0
 
 ## Handles the clock tick event from the event handler
 ##
