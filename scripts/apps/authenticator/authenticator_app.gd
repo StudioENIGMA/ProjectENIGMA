1fa8687ffc0ebe60510515fec63244e84f5620d6
Erick Oda Coulter <erickodac@hotmail.com>
2026-09-21 21:27:41 -0300

    feat: restyle authenticator app with app header and expiring code state

--- scripts/apps/authenticator/authenticator_app.gd
+++ scripts/apps/authenticator/authenticator_app.gd
@@ -1,19 +1,20 @@
-extends VBoxContainer
+extends Control
 
 const AUTHENTICATOR_INSTANCE_SCENE = preload(
-  "res://scenes/apps/authenticator/authenticator_instance.tscn"
+	"res://scenes/apps/authenticator/authenticator_instance.tscn"
 )
 
+#region CHILDREN NODES REFERENCES
+@export var authenticators_container: VBoxContainer
+#endregion CHILDREN NODES REFERENCES
+
 func _ready() -> void:
-  # Instantiate an authenticator for bank app
-  var bank_authenticator_instance = AUTHENTICATOR_INSTANCE_SCENE.instantiate()
-  bank_authenticator_instance.setup(GameData.App.BANK)
-  add_child(bank_authenticator_instance)
+	# Instantiate an authenticator for bank app
+	var bank_authenticator_instance = AUTHENTICATOR_INSTANCE_SCENE.instantiate()
+	authenticators_container.add_child(bank_authenticator_instance)
+	bank_authenticator_instance.setup(GameData.App.BANK)
 
 ## Updates the validity timer for all children authenticator instances
 func update_codes_validity() -> void:
-  if get_child_count() == 0:
-    return
-
-  for child in get_children():
-    child.update_codes_validity()
+	for child in authenticators_container.get_children():
+		child.update_codes_validity()
